a33ad3454716fe53fa2cd5ce4e2d17e1897d15ea
Kai Grossjohann <kai.grossjohann@epochtimes.de>
2015-02-09 22:20:04 +0100

    Allow editing worklog items.
    Add focus to the list of items, use j/k to move focus.
    Allow e to edit the current entry.
    After editing, just write all of the entries out to disk.

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ initializeApp();
 
 function initializeApp() {
     populateWorklogTable();
-    initializeForm();
     populateEmptyForm();
     initializeKeybindings();
 }
@@ -36,6 +35,7 @@ function populateWorklogTable() {
     var renderedItems = WORKLOG.getItems().map(renderItem);
     function renderItem(w) {
         return TMPL.render('row', {
+            cssClass: getCssClass(w),
             date: formatDate(w.date),
             start: formatTime(w.start),
             end: formatTime(w.end),
@@ -43,6 +43,13 @@ function populateWorklogTable() {
             project: w.project,
             comment: w.comment,
         });
+    }
+    function getCssClass(w) {
+        if (w.isCurrent()) {
+            return 'worklog active';
+        } else {
+            return 'worklog';
+        }
     }
     var renderedList = TMPL.render('list', {
         items: renderedItems,
@@ -58,33 +65,52 @@ function formatTime(date) {
     return tide('hh:mm', date);
 }
 
-function initializeForm() {
-    var addForm = document.getElementById('addform');
-    addForm.addEventListener('submit', addWorklogEntry);
+function initializeAddForm() {
+    var itemForm = document.getElementById('itemform');
+    itemForm.removeEventListener('submit', editWorklogEntry);
+    itemForm.addEventListener('submit', addWorklogEntry);
+}
+
+function initializeEditForm() {
+    var itemForm = document.getElementById('itemform');
+    itemForm.removeEventListener('submit', addWorklogEntry);
+    itemForm.addEventListener('submit', editWorklogEntry);
 }
 
 function populateEmptyForm() {
-    var addForm = document.getElementById('addform');
-    addForm.reset();
+    var itemForm = document.getElementById('itemform');
+    itemForm.reset();
     var now = new Date();
-    addForm.elements.formDate.valueAsDate = now;
+    itemForm.elements.formDate.valueAsDate = now;
     var start = new Date(now);
     start.setHours(8);
     start.setMinutes(0);
     start.setSeconds(0);
     start.setMilliseconds(0);
-    addForm.elements.formStart.valueAsDate = start;
+    itemForm.elements.formStart.valueAsDate = start;
     var end = new Date(now);
     end.setHours(16);
     end.setMinutes(0);
     end.setSeconds(0);
     end.setMilliseconds(0);
-    addForm.elements.formEnd.valueAsDate = end;
+    itemForm.elements.formEnd.valueAsDate = end;
+    initializeAddForm();
+}
+
+function populateFormWithItem(w) {
+    var itemForm = document.getElementById('itemform');
+    itemForm.reset();
+    itemForm.elements.formDate.valueAsDate = w.date;
+    itemForm.elements.formStart.valueAsDate = w.start;
+    itemForm.elements.formEnd.valueAsDate = w.end;
+    itemForm.elements.formProject.value = w.project;
+    itemForm.elements.formComment.value = w.comment;
+    initializeEditForm();
 }
 
 function addWorklogEntry(ev) {
-    var addForm = ev.target;
-    var elements = addForm.elements;
+    var itemForm = ev.target;
+    var elements = itemForm.elements;
     var rawDate = elements.formDate.valueAsDate;
     var rawStart = elements.formStart.valueAsDate;
     var rawEnd = elements.formEnd.valueAsDate;
@@ -117,10 +143,48 @@ function addWorklogEntry(ev) {
     return false;
 }
 
+function editWorklogEntry(ev) {
+    var itemForm = ev.target;
+    var elements = itemForm.elements;
+    var rawDate = elements.formDate.valueAsDate;
+    var rawStart = elements.formStart.valueAsDate;
+    var rawEnd = elements.formEnd.valueAsDate;
+    var rawProject = elements.formProject.value;
+    var rawComment = elements.formComment.value;
+
+    var date = rawDate;
+    var start = new Date(date);
+    start.setHours(rawStart.getHours());
+    start.setMinutes(rawStart.getMinutes());
+    var end = new Date(date);
+    end.setHours(rawEnd.getHours());
+    end.setMinutes(rawEnd.getMinutes());
+
+    var item = WORKLOG.getCurrentItem();
+    item.date = date;
+    item.start = start;
+    item.end = end;
+    item.project = rawProject;
+    item.comment = rawComment;
+
+    WORKLOG.saveAllItems();
+
+    populateWorklogTable();
+    populateEmptyForm();
+
+    $('#mainapp a[href="#worklog"]').tab('show');
+
+    ev.preventDefault();
+    return false;
+}
+
 function initializeKeybindings() {
     listener.simple_combo('w', keyWorklog);
     listener.simple_combo('a', keyAddWorklogEntry);
+    listener.simple_combo('e', keyEditWorklogEntry);
     listener.simple_combo('r', keyReports);
+    listener.simple_combo('j', keyNextWorklogEntry);
+    listener.simple_combo('k', keyPrevWorklogEntry);
     $('input[type=text]')
         .bind("focus", function() { listener.stop_listening(); })
         .bind("blur", function() { listener.listen(); });
@@ -131,10 +195,26 @@ function keyWorklog(ev) {
 }
 
 function keyAddWorklogEntry(ev) {
-    $('a.tab-add').tab('show');
-    $('input.addform-date').focus();
+    $('a.tab-item').tab('show');
+    $('input.itemform-date').focus();
+}
+
+function keyEditWorklogEntry(ev) {
+    populateFormWithItem(WORKLOG.getCurrentItem());
+    $('a.tab-item').tab('show');
+    $('input.itemform-date').focus();
 }
 
 function keyReports(ev) {
     $('a.tab-reports').tab('show');
+}
+
+function keyNextWorklogEntry(ev) {
+    WORKLOG.nextItem();
+    populateWorklogTable();
+}
+
+function keyPrevWorklogEntry(ev) {
+    WORKLOG.prevItem();
+    populateWorklogTable();
 }
--- backend.js
+++ backend.js
@@ -3,50 +3,86 @@ var fs = require('fs');
 var os = require('os');
 var tide = require('tide');
 
-function Worklog(file) {
+exports.Worklog = function(file) {
     this.file = file;
     this.data = undefined;
     this.error = undefined;
     this.initialized = false;
     this.readData(this.file); // async
-}
-exports.Worklog = Worklog;
+};
 
-function readData(file) {
+exports.Worklog.prototype.readData = function(file) {
+    var self = this;
     var data = fs.readFileSync(file, { encoding: 'utf8' });
     var lines = data.split(/\r?\n/);
     lines = lines.filter(function (element) { return element; });
+    var initial_current = 0;
     this.data = lines.map(createWorklogItem);
-    function createWorklogItem(line, index) {
-        return exports.WorkItemFromLine(line, index);
+    this.current = undefined;
+    if (this.data && this.data.length >= 1) {
+        this.current = 0;
     }
-}
-Worklog.prototype.readData = readData;
+    function createWorklogItem(line, index, initial_current) {
+        return exports.WorkItemFromLine(line, index, self);
+    }
+};
 
-function getItems() {
+exports.Worklog.prototype.saveAllItems = function() {
+    var items = this.getItems();
+    var lines = items.map(function (x) {
+        return x.toLine()
+    });
+    var data = lines.join('');
+    fs.writeFileSync(this.file, data);
+}
+
+exports.Worklog.prototype.getItems = function() {
     return this.data;
-}
-Worklog.prototype.getItems = getItems;
+};
 
-function addItem(item) {
+exports.Worklog.prototype.getLength = function() {
+    return this.data.length;
+};
+
+exports.Worklog.prototype.addItem = function(item) {
     this.data.push(item);
     var line = item.toLine();
     fs.appendFileSync(this.file, line, { encoding: 'utf8' });
+};
+
+exports.Worklog.prototype.getCurrent = function() {
+    return this.current;
+};
+
+exports.Worklog.prototype.getCurrentItem = function() {
+    return this.data[this.getCurrent()];
 }
-Worklog.prototype.addItem = addItem;
+
+exports.Worklog.prototype.nextItem = function() {
+    this.current++;
+    if (this.current >= this.getLength()) {
+        this.current = -1 + this.getLength();
+    }
+};
+
+exports.Worklog.prototype.prevItem = function() {
+    this.current--;
+    if (this.current < 0) {
+        this.current = 0;
+    }
+};
 
 // obj = { date: ..., start: ..., end: ..., project: ..., comment: ...}
-function WorkItem(obj) {
+exports.WorkItem = function(obj) {
     obj = obj || {};
     this.date       = obj.date      || undefined;
     this.start      = obj.start     || undefined;
     this.end        = obj.end       || undefined;
     this.project    = obj.project   || undefined;
     this.comment    = obj.comment   || undefined;
-}
-exports.WorkItem = WorkItem;
+};
 
-function WorkItemFromLine(line, index) {
+exports.WorkItemFromLine = function(line, index, worklog) {
     var fields = line.split("\t");
     if (fields.length < 4) {
         throw {
@@ -67,30 +103,31 @@ function WorkItemFromLine(line, index) {
     var rawEnd = fields[2];
     var rawProject = fields[3];
     var rawComment = fields[4];
-    
+
     var rawStartFields = rawStart.split(":");
     var rawStartHours = 0 + rawStartFields[0];
     var rawStartMinutes = 0 + rawStartFields[1];
-    
+
     var rawEndFields = rawEnd.split(":");
     var rawEndHours = 0 + parseInt(rawEndFields[0]);
     var rawEndMinutes = 0 + parseInt(rawEndFields[1]);
-    
+
     var result = new WorkItem();
+    result.worklog = worklog;
+    result.index = index;
     result.date = new Date(rawDate);
-    result['start'] = new Date(result.date.getTime());
-    result['start'].setHours(rawStartHours);
-    result['start'].setMinutes(rawStartMinutes);
-    result['end'] = new Date(result.date.getTime());
-    result['end'].setHours(rawEndHours);
-    result['end'].setMinutes(rawEndMinutes);
+    result.start = new Date(result.date.getTime());
+    result.start.setHours(rawStartHours);
+    result.start.setMinutes(rawStartMinutes);
+    result.end = new Date(result.date.getTime());
+    result.end.setHours(rawEndHours);
+    result.end.setMinutes(rawEndMinutes);
     result.project = rawProject;
     result.comment = rawComment;
     return result;
-}
-exports.WorkItemFromLine = WorkItemFromLine;
+};
 
-function toLine() {
+exports.WorkItem.prototype.toLine = function() {
     var fDate = tide('YYYY-MM-DD', this.date);
     var fStart = tide('hh:mm', this.start);
     var fEnd = tide('hh:mm', this.end);
@@ -99,17 +136,19 @@ function toLine() {
     return [
         fDate, fStart, fEnd, fProject, fComment
     ].join("\t") + os.EOL;
-}
-WorkItem.prototype.toLine = toLine;
+};
 
-function durationMinutes() {
+exports.WorkItem.prototype.durationMinutes = function() {
     var millis = this.end.getTime() - this.start.getTime();
     return ( millis / 1000 / 60 );
-}
-WorkItem.prototype.durationMinutes = durationMinutes;
+};
 
-function durationHours() {
+exports.WorkItem.prototype.durationHours = function() {
     var millis = this.end.getTime() - this.start.getTime();
     return ( millis / 1000 / 60 / 60 );
-}
-WorkItem.prototype.durationHours = durationHours;
+};
+
+exports.WorkItem.prototype.isCurrent = function() {
+    var current = this.worklog.getCurrent();
+    return (this.index == current);
+};
